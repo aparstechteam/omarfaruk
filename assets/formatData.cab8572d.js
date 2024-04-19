a9584fb0da078f308f6929dcd21356f29f24eef1
nasimulhasan <nasimulhasan@gmail.com>
2024-04-19 20:59:55 +0600

    Updated system

--- assets/formatData.cab8572d.js
+++
@@ -1,1 +0,0 @@
-const r=t=>Intl.NumberFormat("bn-BD").format(t),n=t=>new Date(t).toLocaleDateString("bn-BD",{year:"numeric",month:"long",day:"numeric",hour:"numeric",minute:"numeric"});export{r as a,n as f};
